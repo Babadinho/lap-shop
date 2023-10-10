0717156a8936a8d046b38e6ecfdd947aa1a60430
Donald Iloekwe <donytex4u@gmail.com>
2023-10-10 18:26:52 +0100

    add header + navbar styling

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,12 +1,30 @@
 <template>
-  <h1>Menu</h1>
-  <Link href="/laptop">Main Page</Link>&nbsp;
-  <Link href="/laptop/create">Another Page</Link>
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
+    <div class="container mx-auto">
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg font-medium">
+          <Link :href="route('laptop.index')">Listings</Link>
+        </div>
+        <div class="text-xl text-gray-900 dark:text-gray-200 font-bold text-center">
+          <Link :href="route('laptop.index')">LapShop</Link>
+        </div>
+        <div>
+          <Link :href="route('laptop.create')" class="btn-primary">+ New Listing</Link>
+        </div>
+      </nav>
+    </div>
+  </header>
 
   <div v-if="flashSuccess" class="success">
     {{ flashSuccess }}
   </div>
-  <slot>Default</slot>
+
+  <main class="container mx-auto p-4 w-full">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+      {{ flashSuccess }}
+    </div>
+    <slot>Default</slot>
+  </main>
 </template>
 
 <script setup>
@@ -16,11 +34,3 @@ import {Link, usePage} from '@inertiajs/vue3'
 const page = usePage()
 const flashSuccess = computed( () => page.props.flash.success)
 </script>
-
-
-<style scoped>
-.success {
-  background-color: green;
-  color: white;
-}
-</style>
