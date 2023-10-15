6fcfda0f354f502cc58e335f815e29ed0775b9cc
Donald Iloekwe <donytex4u@gmail.com>
2023-10-15 13:10:18 +0100

    style laptop show page

--- resources/js/Pages/Laptop/Index.vue
+++ resources/js/Pages/Laptop/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-    <LaptopCard v-for="laptop in laptops" :key="laptop.id">
+    <Box v-for="laptop in laptops" :key="laptop.id">
       <div>
         <Link :href="route('laptop.show', laptop.id)">
           <LaptopData :laptop="laptop" class="text-lg font-bold" /> <LaptopSpec :laptop="laptop" class="text-gray-500" /> <Price :price="laptop.price" class="text-xl" />
@@ -12,14 +12,14 @@
       <div>
         <Link :href="route('laptop.destroy', laptop.id)" method="DELETE">Delete</Link>
       </div>
-    </LaptopCard>
+    </Box>
   </div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
 import LaptopData from '@/Components/LaptopData.vue'
-import LaptopCard from '@/Components/LaptopCard.vue'
+import Box from '@/Components/Box.vue'
 import LaptopSpec from '@/Components/LaptopSpec.vue'
 import Price from '@/Components/Price.vue'
 
--- resources/js/Pages/Laptop/Show.vue
+++ resources/js/Pages/Laptop/Show.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    <LaptopData :laptop="laptop" />
+  <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+    <Box class="md:col-span-7 flex items-center">
+      <div class="w-full text-center font-medium text-gray-500">No images</div>
+    </Box>
+    <div class="md:col-span-5 flex flex-col gap-4">
+      <Box>
+        <template #header>
+          Basic info
+        </template>
+        <LaptopData :laptop="laptop" class="text-lg font-bold" /> <LaptopSpec :laptop="laptop" class="text-gray-500" /> <Price :price="laptop.price" class="text-xl" />
+      </Box>
+
+      <Box>
+        <template #header>
+          Offer
+        </template>
+        Make an offer
+      </Box>
+    </div>
   </div>
 </template>
 
 
 <script setup>
+import Box from '@/Components/Box.vue'
 import LaptopData from '@/Components/LaptopData.vue'
+import LaptopSpec from '@/Components/LaptopSpec.vue'
+import Price from '@/Components/Price.vue'
 defineProps({
   laptop: Object,
 })
