5f927a24fdb163eb319027a82d35d7042b929bdd
Donald Iloekwe <donytex4u@gmail.com>
2023-10-20 15:10:07 +0100

    style edit form

--- resources/js/Pages/Laptop/Edit.vue
+++ resources/js/Pages/Laptop/Edit.vue
@@ -1,80 +1,81 @@
 <template>
   <form @submit.prevent="edit">
-    <div>
-      <div>
-        <label>Brand</label>
-        <input v-model="form.brand" type="text" />
-        <div v-if="form.errors.brand">
+    <div class="grid md:grid-cols-6 gap-4">
+      <div class="col-span-6 md:col-span-3">
+        <label class="label">Brand</label>
+        <input v-model="form.brand" type="text" class="input" />
+        <div v-if="form.errors.brand" class="input-error text-red-500">
           {{ form.errors.brand }}
         </div>
       </div>
     
-      <div>
-        <label>Model</label>
-        <input v-model="form.model" type="text" />
-        <div v-if="form.errors.model">
+      <div class="col-span-6 md:col-span-3">
+        <label class="label">Model</label>
+        <input v-model="form.model" type="text" class="input" />
+        <div v-if="form.errors.model" class="input-error text-red-500">
           {{ form.errors.model }}
         </div>
       </div>
-    
-      <div>
-        <label>Processor</label>
-        <input v-model="form.processor" type="text" />
-        <div v-if="form.errors.processor">
+
+      <div class="col-span-6 md:col-span-3">
+        <label class="label">Processor</label>
+        <input v-model="form.processor" type="text" class="input" />
+        <div v-if="form.errors.processor" class="input-error text-red-500">
           {{ form.errors.processor }}
         </div>
       </div>
-    
-      <div>
-        <label>Ram</label>
-        <input v-model="form.ram" type="text" />
-        <div v-if="form.errors.ram">
+
+      <div class="col-span-6 md:col-span-3">
+        <label class="label">Ram</label>
+        <input v-model="form.ram" type="text" class="input" />
+        <div v-if="form.errors.ram" class="input-error text-red-500">
           {{ form.errors.ram }}
         </div>
       </div>
-    
-      <div>
-        <label>Storage</label>
-        <input v-model="form.storage" type="text" />
-        <div v-if="form.errors.storage">
+
+      <div class="col-span-6 md:col-span-2">
+        <label class="label">Storage</label>
+        <input v-model="form.storage" type="text" class="input" />
+        <div v-if="form.errors.storage" class="input-error text-red-500">
           {{ form.errors.storage }}
         </div>
       </div>
-    
-      <div>
-        <label>Graphics Card</label>
-        <input v-model="form.graphics_card" type="text" />
-        <div v-if="form.errors.graphics_card">
+
+      <div class="col-span-6 md:col-span-2">
+        <label class="label">Graphics Card</label>
+        <input v-model="form.graphics_card" type="text" class="input" />
+        <div v-if="form.errors.graphics_card" class="input-error text-red-500">
           {{ form.errors.graphics_card }}
         </div>
       </div>
-    
-      <div>
-        <label>Condition</label>
-        <input v-model="form.condition" type="text" />
-        <div v-if="form.errors.condition">
+
+      <div class="col-span-6 md:col-span-2">
+        <label class="label">Condition</label>
+        <input v-model="form.condition" type="text" class="input" />
+        <div v-if="form.errors.condition" class="input-error text-red-500">
           {{ form.errors.condition }}
         </div>
       </div>
-    
-      <div>
-        <label>Features</label>
-        <input v-model="form.features" type="text" />
-        <div v-if="form.errors.features">
+
+      <div class="col-span-6 md:col-span-4">
+        <label class="label">Features</label>
+        <input v-model="form.features" type="text" class="input" />
+        <div v-if="form.errors.features" class="input-error text-red-500">
           {{ form.errors.features }}
         </div>
       </div>
-  
-      <div>
-        <label>Price</label>
-        <input v-model.number="form.price" type="text" />
-        <div v-if="form.errors.price">
+
+      
+      <div class="col-span-6 md:col-span-2">
+        <label class="label">Price</label>
+        <input v-model.number="form.price" type="text" class="input" />
+        <div v-if="form.errors.price" class="input-error text-red-500">
           {{ form.errors.price }}
         </div>
       </div>
-    
-      <div>
-        <button type="submit">Edit</button>
+
+      <div class="col-span-6">
+        <button type="submit" class="button-dark">Edit Listing</button>
       </div>
     </div>
   </form>
